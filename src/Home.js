d43f8e99c864addb8b52d5d12a4ed600cde1bb4a
kagisomakgaba <kagisomakgaba99@gmail.com>
2024-06-14 20:48:32 +0200

    Homepage

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <Nav />
+            <h1>Hey Daniel</h1>
+            </div>
+    );
+}
